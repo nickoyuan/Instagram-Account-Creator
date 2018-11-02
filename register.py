7175864f59f541026641da2ab1e31e4e34c16084
Tyler <41879101+tydegennaro@users.noreply.github.com>
2018-11-02 02:36:55 -0400

    Update register.py
    Added more user friendly functionality.

--- register.py
+++ register.py
@@ -1,9 +1,13 @@
 import requests
 import time
+import random
+import string
+import names
 
 class register():
-    def __init__(self, email, username, password, proxy):
+    def __init__(self, email, name, username, password, proxy):
         self.email = email
+        self.name = name
         self.username = username
         self.password = password
         self.user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
@@ -14,7 +18,7 @@ class register():
             'email' : self.email,
             'password' : self.password,
             'username' : self.username,
-            'first_name' : '',
+            'first_name' : self.name,
             'seamless_login_enabled' : '1',
             'tos_version' : 'row',
             'opt_into_one_tap' : 'false'
@@ -37,27 +41,40 @@ class register():
         )
         self.session.headers.update({'x-csrftoken' : start_session.cookies['csrftoken']})
         print(send_request.text)
-        print(send_request.cookies['csrftoken'])
+        print("------------------------------")
 
-def creator_handler():
+def creator_handler(sleep):
     log_file = open('usernames.txt', 'a')
-    password = ""
-    i = 0
     while True:
-        username = "" + str(i)
+        username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+        password = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+        name = names.get_full_name()
         email = username + "@gmail.com"
         proxy = None
-        create_account = register(email, username, password, proxy)
+        create_account = register(email, name, username, password, proxy)
         create_account.register_account()
-        log_file.write(username + "\n")
+        log_file.write(name + " : " + "@" + username + " : " + password +  "\n")
         log_file.flush()
-        print(username)
-        time.sleep(60)
-        i = i + 1
+        print("Name: " + name)
+        print("Email:" + email)
+        print("Username: @" + username)
+        print("Password: " + password)
+        print("Speed: " + str(sleep) + " milliseconds.")
+        print("------------------------------")
+        time.sleep(sleep)
     log_file.close()
 
 def main():
-    creator_handler()
+    print("Instagram Account Creator")
+    print("To quit at any time, press ctrl-z at anytime.")
+    user_speed = input("Please enter a delay in milliseconds: ")
+
+    try:
+        handler_speed = float(user_speed)
+        creator_handler(handler_speed)
+    except ValueError:
+        print("Not a float value.")
+        main()
  
 if __name__ == '__main__':
     main()
