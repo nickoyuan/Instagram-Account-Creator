c15a4be74ea287bb14c93e77774a0aa4933ef0aa
Tyler <41879101+tydegennaro@users.noreply.github.com>
2018-11-02 13:16:09 -0400

    Added proxy support

--- register.py
+++ register.py
@@ -13,7 +13,7 @@ class register():
         self.user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
         self.register_url = 'https://www.instagram.com/accounts/web_create_ajax/'
         self.referer_url = 'https://www.instagram.com/'
-        self.proxy = None
+        self.proxy = proxy
         self.post_data = {
             'email' : self.email,
             'password' : self.password,
@@ -44,7 +44,7 @@ class register():
         print("------------------------------")
 
 def creator_handler(sleep):
-    log_file = open('usernames.txt', 'a')
+    log_file = open('accounts.txt', 'a')
     while True:
         username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
         password = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
@@ -64,16 +64,56 @@ def creator_handler(sleep):
         time.sleep(sleep)
     log_file.close()
 
+def creator_handler_proxies(sleep, proxyfile):
+    log_file = open('accounts.txt', 'a')
+    with open(proxyfile, "r") as proxy_file:
+        proxies = proxy_file.readlines()
+    proxies = [x.strip() for x in proxies]
+    i = 0
+    while True:
+        username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+        password = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+        name = names.get_full_name()
+        email = username + "@gmail.com"
+        proxy = proxies[i]
+        create_account = register(email, name, username, password, proxy)
+        create_account.register_account()
+        log_file.write(name + " : " + "@" + username + " : " + password +  "\n")
+        log_file.flush()
+        print("Name: " + name)
+        print("Email:" + email)
+        print("Username: @" + username)
+        print("Password: " + password)
+        print("Speed: " + str(sleep) + " milliseconds.")
+        print("Proxy: " + proxies[i])
+        print("------------------------------")
+        time.sleep(sleep)
+        i = i + 1
+    log_file.close()
+
 def main():
     print("Instagram Account Creator")
     print("Press ctrl-z to quit at anytime.")
-    user_speed = input("Please enter a delay in milliseconds: ")
-
-    try:
-        handler_speed = float(user_speed)
-        creator_handler(handler_speed)
-    except ValueError:
-        print("Not a float value.")
+    print("Are you using proxies? (y/n)")
+    if (input() == "y"):
+        print("Please enter a delay in milliseconds: ")
+        user_speed = input()
+        try:
+            handler_speed = float(user_speed)
+            creator_handler_proxies(handler_speed, 'proxies.txt')
+        except ValueError:
+            print("Not a float value.")
+            main()
+    elif (input() == "n"):
+        print("Please enter a delay in milliseconds: ")
+        user_speed = input()
+        try:
+            handler_speed = float(user_speed)
+            creator_handler(handler_speed)
+        except ValueError:
+            print("Not a float value.")
+            main()
+    else:
         main()
  
 if __name__ == '__main__':
