b99526034606b276134f31736160e9b7a69b9d42
Tyler <41879101+tydegennaro@users.noreply.github.com>
2018-11-06 12:25:30 -0500

    Add files via upload
    - Cleaned up code
    - Broke the existing program down into multiple modules

---
+++ creator.py
@@ -0,0 +1,57 @@
+"""
+Author: Tyler DeGennaro
+Date: 11/5/18
+creator.py
+
+This is the creator module. The creator module inititates a web session for the creation of the accounts.
+A JSON response is returned by the register account method, which is used to determine in the handler class,
+if an account is successfully created or not.
+"""
+
+# Library imports
+import requests
+import json
+
+# Account creator class
+class Account_Creator():
+
+    # Constructor
+    def __init__(self, email, name, username, password, proxy):
+        self.email = email
+        self.name = name
+        self.username = username
+        self.password = password
+        self.user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        self.register_url = 'https://www.instagram.com/accounts/web_create_ajax/'
+        self.referer_url = 'https://www.instagram.com/'
+        self.proxy = proxy
+        self.post_data = {
+            'email' : self.email,
+            'password' : self.password,
+            'username' : self.username,
+            'first_name' : self.name,
+            'seamless_login_enabled' : '1',
+            'tos_version' : 'row',
+            'opt_into_one_tap' : 'false'
+        }
+        self.session = requests.Session()
+
+    # Register account method
+    def register_account(self):
+        start_session = self.session.get(
+            self.register_url, 
+            proxies={'http' : self.proxy, 'https' : self.proxy}
+        )
+        self.session.headers.update({
+            'referer' : self.referer_url,
+            'x-csrftoken' : start_session.cookies['csrftoken']
+        })
+        send_request = self.session.post(self.register_url,
+        data=self.post_data, 
+        allow_redirects=True, 
+        proxies={'http': self.proxy, 'https': self.proxy}
+        )
+        self.session.headers.update({'x-csrftoken' : start_session.cookies['csrftoken']})
+        response_text = send_request.text
+        response_json = json.loads(response_text)
+        return(response_json)
---
+++ handler.py
@@ -0,0 +1,94 @@
+"""
+Author: Tyler DeGennaro
+Date: 11/5/18
+handler.py
+
+This is the main module. The handler module inititates the creation of the accounts, 
+and reading and writing to .txt files.
+"""
+
+
+# Library imports
+from creator import Account_Creator
+import time
+import random
+import string
+import names
+
+# Handler class
+class Handler():
+    
+    # Constructor
+    def __init__(self, proxy_file):
+        self.first_name = names.get_first_name()
+        self.last_name = names.get_last_name()
+        self.name = self.first_name + " " + self.last_name
+        self.username = self.first_name +  self.last_name + str(random.randint(100,999)) + self.first_name
+        self.password = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+        self.email = self.username + "@gmail.com"
+        self.proxy_file = proxy_file
+    
+    # Write to file method
+    def writer(self, delay, proxy):
+        account_file = open('accounts.txt', 'a')
+        Create_Account = Account_Creator(self.email, self.name, self.username, self.password, proxy)
+        i = 0
+        while True:
+            response = Create_Account.register_account()
+            print(response)
+            if(response['account_created'] == True):
+                account_file.write("Email: " + self.email + '\n')
+                account_file.write("Username: " + self.username + '\n')
+                account_file.write("Password: " + self.password + '\n')
+                account_file.write("" + '\n')
+                account_file.flush()
+                print("------------------------------")
+                print("Name: " + self.name)
+                print("Email:" + self.email)
+                print("Username: @" + self.username)
+                print("Password: " + self.password)
+                print("Proxy: " + str(proxy))
+                print("Speed: " + str(delay) + " milliseconds.")
+                print("------------------------------")
+            elif(response['account_created'] == False or response['message']['status'] == 'fail'):
+                print("------------------------------")
+                print("IP address has been flagged.")
+                print("Please wait awhile, or use a different proxy.")
+                print("Proxy: " + str(proxy))
+                print("Speed: " + str(delay) + " milliseconds.")
+                print("------------------------------")
+            time.sleep(delay)
+            i = i + 1
+            print("Attempt: " + str(i))
+        account_file.close()
+                
+    
+    # Handler method
+    def start_handler(self, proxy, delay):
+        if(proxy != None):
+            self.writer(delay, proxy)
+        else:
+            self.writer(delay, None)
+
+# Main method
+def main():
+    print("Are you using proxies? y/n: ")
+    proxy_input = input()
+    print("Enter a delay in milliseconds: ")
+    delay_input = float(input())
+    if(proxy_input.lower() == 'y'):
+        Account_Handler = Handler("proxies.txt")
+        with open(Account_Handler.proxy_file, 'r') as proxy_file:
+            proxies = proxy_file.readlines()
+        proxies = [line.strip() for line in proxies]
+        Account_Handler.start_handler(proxies[0], delay_input)
+    elif(proxy_input.lower() == 'n'):
+        Account_Handler = Handler(None)
+        Account_Handler.start_handler(None, delay_input)
+    elif(proxy_input.lower() != 'y' and 'n'):
+        print("You did not enter a valid command.")
+        main()
+
+# Main module
+if __name__ == "__main__":
+    main()
