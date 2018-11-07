bad1736b7ca4e62c826daab706a8fa02ed9b1e8e
Tyler <41879101+tydegennaro@users.noreply.github.com>
2018-11-06 23:54:57 -0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -20,54 +20,54 @@ class Handler():
     
     # Constructor
     def __init__(self, proxy_file):
-        self.first_name = names.get_first_name()
-        self.last_name = names.get_last_name()
-        self.name = self.first_name + " " + self.last_name
-        self.username = self.first_name +  self.last_name + str(random.randint(100,999)) + self.first_name
-        self.password = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-        self.email = self.username + "@gmail.com"
         self.proxy_file = proxy_file
     
     # Write to file method
     def writer(self, delay, proxy):
         account_file = open('accounts.txt', 'a')
-        Create_Account = Account_Creator(self.email, self.name, self.username, self.password, proxy)
-        i = 0
         while True:
-            try:
-                response = Create_Account.register_account()
-                print(response)
-                if(response['account_created'] == True):
-                    account_file.write("Email: " + self.email + '\n')
-                    account_file.write("Username: @" + self.username + '\n')
-                    account_file.write("Password: " + self.password + '\n')
-                    account_file.write(" " + '\n')
-                    account_file.flush()
-                    print("------------------------------")
-                    print("Name: " + self.name)
-                    print("Email:" + self.email)
-                    print("Username: @" + self.username)
-                    print("Password: " + self.password)
-                    print("Proxy: " + str(proxy))
-                    print("Speed: " + str(delay) + " milliseconds.")
-                    print("------------------------------")
-                elif(response['account_created'] == False or response['message']['status'] == 'fail'):
-                    print("------------------------------")
-                    print("IP address has been flagged.")
-                    print("Please wait awhile, or use a different proxy.")
-                    print("Proxy: " + str(proxy))
-                    print("Speed: " + str(delay) + " milliseconds.")
-                    print("------------------------------")
-                time.sleep(delay)
-                i = i + 1
-                print("Attempt: " + str(i))
-            except UnboundLocalError as e:
-                print("Failed to load JSON response.")
-                print(e)
-                break
+            i = 0
+            first_name = names.get_first_name()
+            last_name = names.get_last_name()
+            name = first_name + " " + last_name
+            username = first_name +  last_name + str(random.randint(100,999)) + first_name
+            password = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+            email = username + "@gmail.com"
+            for _ in range(len(proxy)):
+                Create_Account = Account_Creator(email, name, username, password, proxy[i])
+                try:
+                    response = Create_Account.register_account()
+                    print(response)
+                    if(response['account_created'] == True):
+                        account_file.write("Email: " + email + '\n')
+                        account_file.write("Username: @" + username + '\n')
+                        account_file.write("Password: " + password + '\n')
+                        account_file.write(" " + '\n')
+                        account_file.flush()
+                        print("------------------------------")
+                        print("Name: " + name)
+                        print("Email:" + email)
+                        print("Username: @" + username)
+                        print("Password: " + password)
+                        print("Proxy: " + str(proxy[i]))
+                        print("Speed: " + str(delay) + " milliseconds.")
+                        print("------------------------------")
+                    elif(response['account_created'] == False or response['message']['status'] == 'fail'):
+                        print("------------------------------")
+                        print("IP address has been flagged.")
+                        print("Please wait awhile, or use a different proxy.")
+                        print("Proxy: " + str(proxy[i]))
+                        print("Speed: " + str(delay) + " milliseconds.")
+                        print("------------------------------")
+                    time.sleep(delay)
+                    i = i + 1
+                    print("Attempt: " + str(i))
+                except UnboundLocalError as e:
+                    print("Failed to load JSON response.")
+                    print(e)
+                    break
         account_file.close()
                 
-    
     # Handler method
     def start_handler(self, proxy, delay):
         try:
@@ -86,11 +86,11 @@ def main():
     print("Enter a delay in milliseconds: ")
     delay_input = float(input())
     if(proxy_input.lower() == 'y'):
-        Account_Handler = Handler("proxiestwo.txt")
+        Account_Handler = Handler("proxies.txt")
         with open(Account_Handler.proxy_file, 'r') as proxy_file:
             proxies = proxy_file.readlines()
         proxies = [line.strip() for line in proxies]
-        Account_Handler.start_handler(proxies[0], delay_input)
+        Account_Handler.start_handler(proxies, delay_input)
     elif(proxy_input.lower() == 'n'):
         Account_Handler = Handler(None)
         Account_Handler.start_handler(None, delay_input)
